862dc7ed9255b7d2164eefbf77e82bc10108f0fd
gamingdyno21 <salunkesourav1224@gmail.com>
2026-04-26 21:35:16 +0530

    Fix Express 5 wildcard routing error

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use('/api/leads', leadRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 }
